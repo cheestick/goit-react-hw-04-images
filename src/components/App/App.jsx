e3ca4e5b5d0e44b30301df528faf2357e591fce8
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-04 12:06:58 +0300

    app init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import s from './App.module.css';
+
+export default class App extends Component {
+  render() {
+    return <div className={s.wrapper}>React homework template</div>;
+  }
+}
