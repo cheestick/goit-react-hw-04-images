c173b332ec9b84ec2726dfd87c4ccd4bc8759591
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-07 16:23:22 +0300

    messed up

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,69 +6,85 @@ import s from './ImageGallery.module.css';
 import * as api from 'services/search-api';
 
 class ImageGallery extends Component {
-  state = {
-    result: [],
-    status: 'idle',
-  };
-
-  async componentDidUpdate(prevProps, _) {
-    const prevQuery = prevProps.query;
-    const nextQuery = this.props.query;
-
-    if (prevQuery === nextQuery) return;
-
-    this.setState({ status: 'loading' });
-
-    const searchResult = await api.fetchImages(nextQuery);
-    this.setState({
-      result: searchResult.hits,
-      status: 'resolve',
-    });
-    this.page = 2;
+  constructor() {
+    super();
+    this.state = {
+      result: [],
+      status: 'idle',
+    };
   }
 
-  loadMore = async () => {
-    const { query } = this.props;
+  async componentDidUpdate(prevProps, prevState) {
+    // const prevQuery = prevProps.query;
+    const nextQuery = this.props.query;
+    console.count('Did Update: ');
+
+    if (prevState.status === 'idle') {
+      this.setState({ status: 'loading' });
+      const data = await api.fetchMoreImages(nextQuery);
+      this.setState(prevState => ({
+        result: [...prevState.result, ...data.hits],
+        status: 'more',
+      }));
+    }
+
+    // if (prevQuery === nextQuery) {
+    //   const { query } = this.props;
+    //   this.setState({ status: 'loading' });
+    //   const moreResult = await api.fetchImages(query, this.page);
+    //   this.setState(
+    //     prevState => ({
+    //       result: [...prevState.result, ...moreResult.hits],
+    //       status: 'more',
+    //     }),
+    //     this.smoothScroll
+    //   );
+    //   return;
+    // }
+
+    // if (prevState.status === this.state.status) return;
     // this.setState({ status: 'loading' });
-    const moreResult = await api.fetchImages(query, this.page);
-    this.page += 1;
-    this.setState(
-      prevState => ({
-        result: [...prevState.result, ...moreResult.hits],
-        // status: 'resolve',
-      }),
-      () => {
-        window.scrollBy({
-          top: document.documentElement.clientHeight * this.page,
-          behavior: 'smooth',
-        });
-      }
-    );
+    // const searchResult = await api.fetchImages(nextQuery, this.page);
+    // this.setState(
+    //   prevState => ({
+    //     result: [...prevState.result, ...searchResult.hits],
+    //     status: 'more',
+    //   }),
+    //   this.smoothScroll
+    // );
+  }
+
+  loadMore = () => {
+    this.setState({ status: 'loading' });
+  };
+
+  smoothScroll = () => {
+    window.scrollBy({
+      top: document.documentElement.clientHeight * this.page,
+      behavior: 'smooth',
+    });
   };
 
   render() {
     const { result, status } = this.state;
     const { showModal } = this.props;
-
-    if (status === 'idle') return null;
-
-    if (status === 'loading') return <Loader />;
-
-    if (status === 'resolve')
-      return (
-        <>
-          <ul className={s.gallery}>
-            {result.map(galleryItemData => (
-              <ImageGalleryItem
-                key={galleryItemData.id}
-                data={galleryItemData}
-                showModal={showModal}
-              />
-            ))}
-          </ul>
+    return (
+      <>
+        <ul className={s.gallery}>
+          {result.map(galleryItemData => (
+            <ImageGalleryItem
+              key={galleryItemData.id}
+              data={galleryItemData}
+              showModal={showModal}
+            />
+          ))}
+        </ul>
+        {status === 'loading' && <Loader />}
+        {status === 'more' && (
           <Button label="Load More" onClick={this.loadMore} />
-        </>
-      );
+        )}
+      </>
+    );
   }
 }
 
--- src/services/search-api.js
+++ src/services/search-api.js
@@ -24,3 +24,40 @@ export const fetchImages = async (query = '', page = 1) => {
   }
   return Promise.reject(new Error(`${query} response is not OK!`));
 };
+
+const currentQuery = {
+  query: '',
+  page: 0,
+  totalPages: null,
+  lastFetch: false,
+};
+
+export const fetchMoreImages = async query => {
+  !isNewQuery(query) && !isLastPage() && increasePageNumber();
+  isNewQuery(query) && initWithNewQuery(query);
+  isLastPage() && setLastFetch();
+  if (isLastFetch()) return [];
+
+  const data = await fetchImages(query, currentQuery.page);
+  nullishTotalPages() && setTotalPageCount(data.totalHits);
+
+  return data;
+};
+
+const initWithNewQuery = query => {
+  currentQuery.query = query;
+  currentQuery.page = 1;
+  currentQuery.totalPages = null;
+  currentQuery.lastFetch = false;
+};
+
+const increasePageNumber = () => (currentQuery.page += 1);
+const setTotalPageCount = totalHits => {
+  currentQuery.totalPages = Math.ceil(totalHits / PER_PAGE);
+};
+
+const setLastFetch = () => (currentQuery.lastFetch = true);
+const nullishTotalPages = () => currentQuery.totalPages === null;
+const isLastFetch = () => currentQuery.lastFetch;
+const isNewQuery = newQuery => currentQuery.query !== newQuery;
+const isLastPage = () => (currentQuery.totalPages ?? 0) >= currentQuery.page;
