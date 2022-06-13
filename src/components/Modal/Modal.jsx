d82da4f0eab170ef70fa85013b7672dfff4d10d9
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-13 22:54:52 +0300

    modal component hooked

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onClose);
-  }
+export default function Modal({ onClose, children }) {
+  const onModalClose = useCallback(
+    e => {
+      (e.code === 'Escape' || e.currentTarget === e.target) && onClose();
+    },
+    [onClose]
+  );
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onClose);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', onModalClose);
+    return () => {
+      window.removeEventListener('keydown', onModalClose);
+    };
+  }, [onModalClose]);
 
-  onClose = e => {
-    (e.code === 'Escape' || e.currentTarget === e.target) &&
-      this.props.onClose();
-  };
-
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.onClose}>
-        <div className={s.modal}>{this.props.children}</div>
-      </div>,
-      document.querySelector('#modal')
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={onModalClose}>
+      <div className={s.modal}>{children}</div>
+    </div>,
+    document.querySelector('#modal')
+  );
 }
 
 Modal.propTypes = {
