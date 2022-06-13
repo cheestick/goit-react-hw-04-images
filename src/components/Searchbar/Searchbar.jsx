085e430884435cb38deb201fce68565d33204b00
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-13 22:14:06 +0300

    searchbar hooked

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import s from './Searchbar.module.css';
 
-export default class Searchbar extends Component {
-  state = {
-    query: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
 
-  onSubmit = e => {
+  const onFormSubmit = e => {
     e.preventDefault();
-    const query = this.state.query.trim();
-    if (query.length < 1) return;
-    this.props.onSubmit(query);
+    const trimmedQuery = query.trim();
+    if (trimmedQuery.length < 1) return;
+
+    onSubmit(trimmedQuery);
   };
 
-  onChange = e => {
-    this.setState({ query: e.target.value });
-  };
+  return (
+    <header className={s.searchbar}>
+      <form className={s.form} onSubmit={onFormSubmit}>
+        <button type="submit" className={s.button}>
+          <span className={s.buttonLabel}>Search</span>
+        </button>
 
-  render() {
-    return (
-      <header className={s.searchbar}>
-        <form className={s.form} onSubmit={this.onSubmit}>
-          <button type="submit" className={s.button}>
-            <span className={s.buttonLabel}>Search</span>
-          </button>
-
-          <input
-            className={s.input}
-            type="text"
-            value={this.state.query}
-            onChange={this.onChange}
-            placeholder="Search images and photos"
-            autoComplete="off"
-            autoFocus
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={s.input}
+          type="text"
+          value={query}
+          onChange={({ target: { value } }) => setQuery(value)}
+          placeholder="Search images and photos"
+          autoComplete="off"
+          autoFocus
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
